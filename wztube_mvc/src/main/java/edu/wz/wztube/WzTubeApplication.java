f66b1ca58fcf398a5ab6e5283bf9c43c74c59644
wizewizzard <kingaal93@gmail.com>
2022-08-28 12:49:36 +0700

    Add mvc module

---
+++ wztube_mvc/src/main/java/edu/wz/wztube/WzTubeApplication.java
@@ -0,0 +1,14 @@
+package edu.wz.wztube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzTubeApplication {
+    
+    
+    public static void main(String[] args) {
+        SpringApplication.run(WzTubeApplication.class, args);
+    }
+    
+}
